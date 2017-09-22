d1a934c80a681b961ddc8d5c632063d8748e766f
Andrew Lam <atlam995@gmail.com>
2017-09-21 21:49:12 -0700

    Added Images.php and _cell.php Simplified welcome.php and gallery.php Signed-off-by:Andrew Lam <atlam995@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,6 +19,26 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+            // get all the images from our model
+            $pix = $this->images->newest();
+            
+            // build an array of formatted cells for them
+            foreach ($pix as $picture)
+                $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+            
+            // prime the table class
+            $this->load->library('table');
+            $parms = array(
+                'table_open' => '<table class="gallery">',
+                'cell_start' => '<td class="oneimage">',
+                'cell_alt_start' => '<td class="oneimage">'
+            );
+            $this->table->set_template($parms);
+            
+            // finally! generate the table
+            $rows = $this->table->make_columns($cells, 3);
+            $this->data['thetable'] = $this->table->generate($rows);
+            
             $this->data['pagebody'] = 'welcome';
             $this->render();
 	}
